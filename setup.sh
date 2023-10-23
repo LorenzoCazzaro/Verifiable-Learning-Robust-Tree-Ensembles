dc72b25f83c20692fee34404d37a38d9ceff5298
LorenzoCazzaro <lorenz.cazzaro@gmail.com>
2023-10-23 17:56:20 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -2,8 +2,12 @@
 
 cd ./src/utils
 
-python3 split_mnist.py
-python3 split_fmnist.py 0 3
-python3 split_rewema.py
-python3 split_webspam.py
+#python3 split_mnist.py
+#python3 split_fmnist.py 0 3
+#python3 split_rewema.py
+#python3 split_webspam.py
 
+cd ../silva/src
+make
+
+cd ../../carve
