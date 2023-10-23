76a8068686016d4b26a1b0418d57b08ff94f4725
LorenzoCazzaro <lorenz.cazzaro@gmail.com>
2023-10-23 18:07:43 +0200

    setup file added

--- setup.sh
+++ setup.sh
@@ -2,12 +2,15 @@
 
 cd ./src/utils
 
-#python3 split_mnist.py
-#python3 split_fmnist.py 0 3
-#python3 split_rewema.py
-#python3 split_webspam.py
+python3 split_mnist.py
+python3 split_fmnist.py 0 3
+python3 split_rewema.py
+python3 split_webspam.py
 
 cd ../silva/src
 make
 
 cd ../../carve
+mkdir build; cd build
+cmake ..
+make
